236a571589641a40a7b74be42103d5466240e4cf
Daniel N Somoano <dnsomoano@gmail.com>
2018-09-18 22:20:29 -0400

    Made poco and added first migration.

---
+++ SafariVacationContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ORM_Safari_.Models;
+
+namespace ORM_Safari_
+{
+    public partial class SafariVacationContext : DbContext
+    {
+        public SafariVacationContext()
+        {
+        }
+
+        public SafariVacationContext(DbContextOptions<SafariVacationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("server=localhost;database=SafariVacation;username=postgres;password=mark");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+
+        public DbSet<SeenAnimals> SeenAnimalsTable { get; set; }
+    }
+}
